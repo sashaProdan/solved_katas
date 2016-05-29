0750a4b4909d75753755957f36f691734bca8c98
Alexandra Prodan <prodan.sasha@gmail.com>
2016-05-28 19:41:58 -0500

    Valid parentheses

--- valid_parentheses.rb
+++ valid_parentheses.rb
@@ -1,4 +1,12 @@
 
+# Write a function called `validParentheses` that takes a string
+# of parentheses, and determines if the order of the parentheses is valid.
+# The method shoul return true if the string is valid, and false if
+# it's invalid.
+# All input strings will be nonempty, and will only consist of open
+# parentheses '('and/or closed ones ')'
+
+
 def valid_parentheses(str)
   str = str.chars.select {|char| char == "(" || char == ")"}.join 
   s = str.gsub(/\(\)/, '')
@@ -6,9 +14,9 @@ def valid_parentheses(str)
   s.empty?
 end
 
-p valid_parentheses( "()" )               # => true
-p valid_parentheses( ")(()))" )           # => false
-p valid_parentheses( "(" )                # => false
-p valid_parentheses( "(())((()())())" )   # => true
-p valid_parentheses("())(")             # => false
-p valid_parentheses("hi(hi)()")           # => true# 
+valid_parentheses( "()" )               # => true
+valid_parentheses( ")(()))" )           # => false
+valid_parentheses( "(" )                # => false
+valid_parentheses( "(())((()())())" )   # => true
+valid_parentheses("())(")               # => false
+valid_parentheses("hi(hi)()")           # => true# 
